423d10eecf4217dc0a8ebc9343feaba9c47f95c1
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2021-01-31 23:22:23 +0100

    currency converter

---
+++ src/CurrencyRow.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+const CurrencyRow = (props) => {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount,
+  } = props;
+  return (
+    <div className="currency-row">
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyRow;
